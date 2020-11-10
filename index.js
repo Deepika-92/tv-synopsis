ce098fe520700d2ddfe44888b2ae2a467f87b66c
Deepika-92 <71193396+Deepika-92@users.noreply.github.com>
2020-11-09 21:42:39 -0500

    Fix issue with uploading to GitHub

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const showdata = require('./showdata')
+
+const app = express()
+
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+
+app.get('/', (request, response) => {
+  console.log(showdata) // eslint-disable-line no-console
+
+  return response.render('mainpage', { showdata })
+})
+
+app.get('/season/:number', (request, response) => {
+  const findSeason = showdata.seasons.find((season) => {
+    return season.number === parseInt(request.params.number)
+  })
+
+  return response.render('seasonpage', { title: showdata.title, season: findSeason })
+})
+
+app.all('*', (request, response) => {
+  response.sendStatus(404)
+})
+
+app.listen(1337, () => {
+  console.log('Listening on 1337...') // eslint-disable-line no-console
+})
